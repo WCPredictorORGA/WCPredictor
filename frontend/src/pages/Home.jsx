4f0346d800810483c33f0b331ec7f270a5d37db8
francoismanoury-wq <francois.manoury@gmail.com>
2026-06-23 21:18:42 +0200

    config de tailwindcss

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold text-blue-600 mb-4">Accueil - Coupe du Monde 2026</h1>
+      <p>Bienvenue sur WCPredictor. Voici les prochains matchs...</p>
+    </div>
+  );
+}
